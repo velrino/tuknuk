c8127f242b7c762007c27f3ac6537a506a532f18
velrino <denismagalhaesvelrino@gmail.com>
2023-02-02 07:35:41 -0300

    :art: Implemented Ant Design

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -1,7 +1,11 @@
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
 import React from 'react';
 
 export default function Loading(){
+    const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
     return(
-        <p>Loading Page</p>
+        <Spin indicator={antIcon} />
     );
 }
